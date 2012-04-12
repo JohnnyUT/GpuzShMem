26decdb8c46e710f764b1bf6b1c284def3b275b4
Johann Binder <johnnyut@gmx.at>
2012-04-12 21:30:02 +0200

    added both projects

---
+++ GpuzDemo/GpuzDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GpuzDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GpuzWrapper gpuz = new GpuzWrapper();
+            gpuz.Open();
+
+            Console.WriteLine(gpuz.ListAllData());
+            Console.WriteLine();
+
+            Console.WriteLine(gpuz.ListAllSensors());
+            Console.WriteLine();
+
+            Console.WriteLine(gpuz.DataKey(0) + ": " + gpuz.DataValue(0));
+            Console.WriteLine(gpuz.SensorName(0) + ": " + gpuz.SensorValue(0) + " " + gpuz.SensorUnit(0));
+
+            gpuz.Close();
+            Console.WriteLine("Press ENTER to quit");
+            Console.ReadLine();
+        }
+    }
+}
